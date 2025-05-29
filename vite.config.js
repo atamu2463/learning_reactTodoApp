e782cd962191c5444c58e6a9273954438210d5b7
asa-cording <asahiwork.cording@gmail.com>
2025-05-30 04:12:40 +0900

    github pagesで公開できるように設定

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+//リポジトリ名を定数で指定
+const REPO_NAME = 'learning_reactTodoApp';
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${REPO_NAME}/`,
+  build: {
+    outDir:'dist', // 出力先ディレクトリを指定
+  },
 })
